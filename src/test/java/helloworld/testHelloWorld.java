07725a141922b097cb93bf089ddb9423d038d049
SravanVenus <103585686+SravanVenus@users.noreply.github.com>
2022-04-19 13:39:38 +0530

    first

--- src/test/java/helloworld/testHelloWorld.java
+++ src/test/java/helloworld/testHelloWorld.java
@@ -1,5 +1,12 @@
 package helloworld;
 
+import org.testng.annotations.Test;
+
 public class testHelloWorld {
+	@Test
+	public void TestHelloWorld() {
+		
+		System.out.println("...Hello-World...");
+	}
 
 }
